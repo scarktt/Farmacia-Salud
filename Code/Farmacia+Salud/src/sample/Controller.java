a5d97087a076057a03748f5daad94d2600788f28
Fatima1305 <fatimacastillo15@gmail.com>
2019-08-04 17:38:47 -0600

    Codigo de login
    Actualizo la primera pantalla, ya no tiene los bordes, ya cierra con la imagen con una X. Pero aún no minimiza.

--- Code/Farmacia+Salud/src/sample/Controller.java
+++ Code/Farmacia+Salud/src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+
 public class Controller {
+    //Cierra el programa
+    public void pressExit(){
+        System.exit(0);
+    }
+    //Minimiza el programa
+    public void pressMin(){
+        //setIconified(1);aun no funciona
+    }
+    //Variables
+    @FXML
+    private TextField txtuser;
+    @FXML
+    private TextField txtpwd;
+    @FXML
+    private Button btnInSesion;
+
+    /*Clase para bloquear el boton de iniciar sesion hasta  que en
+    las cajas de texto se haya escrito algo*/
+    public void keyReleaseProperty(){
+        String user = txtuser.getText();
+        String password = txtpwd.getText();
+        boolean Deshabilitar = (user.isEmpty()|| user.trim().isEmpty()) || (password.isEmpty()|| password.trim().isEmpty());
+        btnInSesion.setDisable(Deshabilitar);
+    }
+
 }
