2ffdd237b75f1e4d31ba6cb7c31de1a741f7bf83
Fatima1305 <fatimacastillo15@gmail.com>
2019-08-08 23:52:55 -0600

    Pedidos Y parte de producto

--- Code/Farmacia+Salud/src/sample/Controlador/FacturacionControlador.java
+++ Code/Farmacia+Salud/src/sample/Controlador/FacturacionControlador.java
@@ -18,14 +18,27 @@ public class FacturacionControlador implements Initializable {
 
     @FXML private AnchorPane APUser;
     @FXML private AnchorPane APNotificacion;
+    @FXML private AnchorPane APVenta;
     @FXML private AnchorPane APFacturacion;
-    @FXML private AnchorPane APBotonesFacturacion;
     @FXML private AnchorPane APDetalleFactura;
     @FXML private AnchorPane APHistoricoVentas;
+    @FXML private AnchorPane APCompra;
     @FXML private AnchorPane APCompras;
     @FXML private AnchorPane APCompras2;
+    @FXML private AnchorPane APDetalleCompra;
+    @FXML private AnchorPane APHistoricoCompras;
+    @FXML private AnchorPane APPedido;
+    @FXML private AnchorPane APPedidos;
+    @FXML private AnchorPane APDetallePedido;
+    @FXML private AnchorPane APHistoricoPedidos;
+    @FXML private AnchorPane APProducto;
+    @FXML private AnchorPane APProductos;
+    @FXML private AnchorPane APHistoricoProducto;
+    //Botones de arriiba
+    @FXML private AnchorPane APBotonesFacturacion;
+    @FXML private AnchorPane APBotonesPedido;
     @FXML private AnchorPane APBotonesCompra;
-    @FXML private ComboBox<Empleado> cmbVendidoPor;
+    @FXML private AnchorPane APBotonesProducto;
 
     private ObservableList<Empleado> ListaVendedores;
 
@@ -71,41 +84,122 @@ public class FacturacionControlador implements Initializable {
         //No funciona
     }
 
-    //Muestra en pantalla la ventana de facturacion
-    public void pressFacturacion(MouseEvent event) {
-        APFacturacion.setVisible(true); APBotonesFacturacion.setVisible(true);
-        APDetalleFactura.setVisible(false); APHistoricoVentas.setVisible(false);
-        APCompras.setVisible(false); APBotonesCompra.setVisible(false);
-        APCompras2.setVisible(false);
-    }
 
-    //Muestra en pantalla la ventana de detalle factura
-    public void pressDetalleFactura(MouseEvent event) {
-      APDetalleFactura.setVisible(true);
-      APFacturacion.setVisible(false); APHistoricoVentas.setVisible(false);
-        APCompras.setVisible(false); APCompras2.setVisible(false);
-    }
-
-    //Muestra en pantalla la ventana de historico de ventas
-    public void pressHistoricoVentas(MouseEvent event) {
-        APHistoricoVentas.setVisible(true);
-        APDetalleFactura.setVisible(false); APFacturacion.setVisible(false);
-        APCompras.setVisible(false); APCompras2.setVisible(false);
-    }
 
     //Muestra en pantalla la ventana de compras
     public void pressCompras(MouseEvent event) {
-        APCompras.setVisible(true); APBotonesCompra.setVisible(true);
-        APHistoricoVentas.setVisible(false);APDetalleFactura.setVisible(false);
-        APFacturacion.setVisible(false); APBotonesFacturacion.setVisible(false);
+        APCompra.setVisible(true);
+        APBotonesCompra.setVisible(true);
+        APCompras.setVisible(true);
+        APVenta.setVisible(false);
+        APHistoricoVentas.setVisible(false);
+        APDetalleFactura.setVisible(false);
+        APFacturacion.setVisible(false);
         APCompras2.setVisible(false);
+        APDetalleCompra.setVisible(false);
+        APHistoricoCompras.setVisible(false);
+        APPedidos.setVisible(false);
+        APPedido.setVisible(false);
+        APDetallePedido.setVisible(false);
+        APHistoricoPedidos.setVisible(false);
+        APHistoricoPedidos.setVisible(false);
+        APProductos.setVisible(false);
+        APProducto.setVisible(false);
+        APBotonesFacturacion.setVisible(false);
+        APBotonesPedido.setVisible(false);
+        APBotonesProducto.setVisible(false);
     }
 
     //Muestra en pantalla la ventana de compras2
     public void pressCompras2(MouseEvent event) {
         APCompras2.setVisible(true);
-        APHistoricoVentas.setVisible(false);APDetalleFactura.setVisible(false);
-        APFacturacion.setVisible(false); APBotonesFacturacion.setVisible(false);
+        APDetalleCompra.setVisible(false);
+        APHistoricoCompras.setVisible(false);
         APCompras.setVisible(false);
+    }
+
+    //Muestra en pantalla la ventana de DetalleCompra
+    public void pressDetalleCompra(MouseEvent event) {
+        APDetalleCompra.setVisible(true);
+        APHistoricoCompras.setVisible(false);
+        APCompras2.setVisible(false);
+        APCompras.setVisible(false);
+    }
+
+    //Muestra en pantalla la ventana de HistoricoCompra
+    public void pressHistoricoCompra(MouseEvent event) {
+        APHistoricoCompras.setVisible(true);
+        APCompras2.setVisible(false);
+        APCompras.setVisible(false);
+        APDetalleCompra.setVisible(false);
+    }
+
+    //Muestra en pantalla la ventana de pedido
+    public void pressPedido(MouseEvent event) {
+        APPedidos.setVisible(true);
+        APPedido.setVisible(true);
+        APBotonesPedido.setVisible(true);
+        APDetallePedido.setVisible(false);
+        APHistoricoPedidos.setVisible(false);
+        APBotonesCompra.setVisible(false);
+        APVenta.setVisible(false);
+        APHistoricoVentas.setVisible(false);
+        APDetalleFactura.setVisible(false);
+        APFacturacion.setVisible(false);
+        APCompra.setVisible(false);
+        APCompras.setVisible(false);
+        APCompras2.setVisible(false);
+        APDetalleCompra.setVisible(false);
+        APHistoricoCompras.setVisible(false);
+        APProductos.setVisible(false);
+        APProducto.setVisible(false);
+        APBotonesCompra.setVisible(false);
+        APBotonesFacturacion.setVisible(false);
+        APBotonesProducto.setVisible(false);
+    }
+
+    //Muestra en pantalla la ventana de Detallepedido
+    public void pressDetallePedido(MouseEvent event) {
+        APDetallePedido.setVisible(true);
+        APHistoricoPedidos.setVisible(false);
+        APPedidos.setVisible(false);
+    }
+
+    //Muestra en pantalla la ventana de Historicopedido
+    public void pressHistoricoPedido(MouseEvent event) {
+        APHistoricoPedidos.setVisible(true);
+        APPedidos.setVisible(false);
+        APDetallePedido.setVisible(false);
+    }
+
+    //Muestra en pantalla la ventana de producto
+    public void pressProducto(MouseEvent event) {
+        APProductos.setVisible(true);
+        APProducto.setVisible(true);
+        APBotonesProducto.setVisible(true);
+        APBotonesCompra.setVisible(false);
+        APVenta.setVisible(false);
+        APHistoricoVentas.setVisible(false);
+        APDetalleFactura.setVisible(false);
+        APFacturacion.setVisible(false);
+        APCompra.setVisible(false);
+        APCompras.setVisible(false);
+        APCompras2.setVisible(false);
+        APDetalleCompra.setVisible(false);
+        APPedidos.setVisible(false);
+        APPedido.setVisible(false);
+        APDetallePedido.setVisible(false);
+        APHistoricoPedidos.setVisible(false);
+        APHistoricoCompras.setVisible(false);
+        APBotonesCompra.setVisible(false);
+        APBotonesFacturacion.setVisible(false);
+        APBotonesPedido.setVisible(false);
+    }
+
+    //Muestra en pantalla la ventana de Historicoproducto
+    public void pressHistoricoProducto(MouseEvent event) {
+        APHistoricoProducto.setVisible(true);
+        APProductos.setVisible(false);
+        APDetallePedido.setVisible(false);
     }
 }
