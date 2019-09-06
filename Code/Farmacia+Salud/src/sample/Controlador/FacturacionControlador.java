a941dc5da1b3598c222d7f55b238129ccfb1327f
Fatima1305 <fatimacastillo15@gmail.com>
2019-09-06 10:48:20 -0600

    Arreglando errores

--- Code/Farmacia+Salud/src/sample/Controlador/FacturacionControlador.java
+++ Code/Farmacia+Salud/src/sample/Controlador/FacturacionControlador.java
@@ -1,22 +1,21 @@
 package sample.Controlador;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import sample.Modelo.*;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class FacturacionControlador implements Initializable {
@@ -37,12 +36,12 @@ public class FacturacionControlador implements Initializable {
     // CheckBox
     @FXML private CheckBox ckboxGenerico;
     // TableView
-    @FXML private TableView<List<StringProperty>> TVAgregarProductos;
+    @FXML private TableView<Producto> TVAgregarProductos;
     // TableView's Columns
-    @FXML private TableColumn<List<StringProperty>, String> TCProducto;
-    @FXML private TableColumn<List<StringProperty>, String> TCProveedor;
-    @FXML private TableColumn<List<StringProperty>, String> TCFormaFarmaceutica;
-    @FXML private TableColumn<List<StringProperty>, String> TCDosis;
+    @FXML private TableColumn<Producto, String> TCProducto;
+    @FXML private TableColumn<Producto, String> TCProveedor;
+    @FXML private TableColumn<Producto, String> TCFormaFarmaceutica;
+    @FXML private TableColumn<Producto, String> TCDosis;
 
     // String Collections for ComboBox Items
     private ObservableList<String> listaVendedores = FXCollections.observableArrayList();
@@ -53,7 +52,7 @@ public class FacturacionControlador implements Initializable {
     private ObservableList<String> listaUtilidad = FXCollections.observableArrayList();
 
     // String Collections for TableView
-    private ObservableList<List<StringProperty>> data = FXCollections.observableArrayList();
+    private ObservableList<Producto> data = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -61,9 +60,10 @@ public class FacturacionControlador implements Initializable {
         conexion.establecerConexion();
 
         LoadDataCmbox(conexion);
+        getListFromDataBaseAndLoadData(conexion);
+        filterList();
 
         conexion.cerrarConexion();
-
     }
 
     private void LoadDataCmbox (Conexion conexion) {
@@ -84,111 +84,46 @@ public class FacturacionControlador implements Initializable {
         cmbUtilidad.setItems(listaUtilidad);
     }
 
-    /************************************ METODO HANDLE PARA EL BUSCADOR ************************************/
+    /************************************ METODO PARA EL BUSCADOR ************************************/
 
-    private void filterList (Conexion conexion, String newValue, String oldValue) {
-        if (buscar.getText().isEmpty()) {
-            data.clear();
+    private void filterList () {
+        FilteredList<Producto> filterData = new FilteredList<>(data, b -> true);
 
-        } else if (newValue.length() < oldValue.length()) {
-            data.clear();
-            getListFromDataBaseAndLoadData (conexion, newValue);
+        buscar.textProperty().addListener((observable, oldValue, newValue) -> filterData.setPredicate(Producto -> {
 
-        } else {
-            data.clear();
-            getListFromDataBaseAndLoadData (conexion, newValue);
-        }
+            if (newValue == null || newValue.isEmpty()) {
+                return true;
+            }
+
+            String lowerCaseFilter = newValue.toLowerCase();
+
+            if (Producto.getNombre().toLowerCase().contains(lowerCaseFilter)) {
+                return true;
+            } else if (Producto.getNombreProveedor().toLowerCase().contains(lowerCaseFilter)) {
+                return true;
+            } else return String.valueOf(Producto.getDosis()).contains(lowerCaseFilter);
+        }));
+
+        SortedList<Producto> sortedData = new SortedList<>(filterData);
+
+        sortedData.comparatorProperty().bind(TVAgregarProductos.comparatorProperty());
+
+        TVAgregarProductos.setItems(sortedData);
     }
 
-    private void getListFromDataBaseAndLoadData (Conexion conexion, String newValue) {
-        Producto.busquedaDinamicaProducto(conexion.getConnection(), newValue, getTipo(), getProveedorValue(),
-                getFormafarmaceuticaValue(), getDosisValue(), getUnidadMedidaValue (), getUtilidadValue (), getIndicacion (), getGenerico (), data);
+    private void getListFromDataBaseAndLoadData (Conexion conexion) {
+        Producto.CargarProductos(conexion.getConnection(), data);
 
         // Enlazar listas con TableView
         TVAgregarProductos.setItems(data);
 
-        // Enlazar columnas con atributos
-        TCProducto.setCellValueFactory(data -> data.getValue().get(1));
-        TCProveedor.setCellValueFactory(data -> data.getValue().get(2));
-        TCFormaFarmaceutica.setCellValueFactory(data -> data.getValue().get(3));
-        TCDosis.setCellValueFactory(data -> data.getValue().get(4));
+        // Llenar los combobox
+        TCProducto.setCellValueFactory(new PropertyValueFactory<>("Nombre"));
+        TCProveedor.setCellValueFactory(new PropertyValueFactory<>("NombreProveedor"));
+        TCFormaFarmaceutica.setCellValueFactory(new PropertyValueFactory<>("FormaFarmaceutica"));
+        TCDosis.setCellValueFactory(new PropertyValueFactory<>("Dosis"));
 
         conexion.cerrarConexion();
-    }
-
-    private String oldValue = "";
-
-    public void handleKeyReleased() {
-        Conexion conexion = new Conexion();
-        conexion.establecerConexion();
-
-        filterList(conexion, buscar.getText(), oldValue);
-
-        oldValue = buscar.getText();
-    }
-
-    public void OnReleasedProveedor() {
-        Conexion conexion = new Conexion();
-        conexion.establecerConexion();
-        data.clear();
-
-        getListFromDataBaseAndLoadData (conexion, buscar.getText());
-    }
-
-    public void OnReleasedFormaF() {
-        Conexion conexion = new Conexion();
-        conexion.establecerConexion();
-        data.clear();
-
-        getListFromDataBaseAndLoadData (conexion, buscar.getText());
-    }
-
-    public void OnReleasedUnidadMedida() {
-        Conexion conexion = new Conexion();
-        conexion.establecerConexion();
-        data.clear();
-
-        getListFromDataBaseAndLoadData (conexion, buscar.getText());
-    }
-
-    public void OnReleasedUtilidad() {
-        Conexion conexion = new Conexion();
-        conexion.establecerConexion();
-        data.clear();
-
-        getListFromDataBaseAndLoadData (conexion, buscar.getText());
-    }
-
-    public void OnReleasedIndicacion() {
-        Conexion conexion = new Conexion();
-        conexion.establecerConexion();
-        data.clear();
-
-        getListFromDataBaseAndLoadData (conexion, buscar.getText());
-    }
-
-    public void OnMouseClickedGenerico() {
-        Conexion conexion = new Conexion();
-        conexion.establecerConexion();
-        data.clear();
-
-        getListFromDataBaseAndLoadData (conexion, buscar.getText());
-    }
-
-    public void OnTableItemSelected() {
-        /*TVAgregarProductos.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        TVAgregarProductos.getSelectionModel().setCellSelectionEnabled(true);*/
-
-        TablePosition tablePosition = TVAgregarProductos.getSelectionModel().getSelectedCells().get(0);
-        int row = tablePosition.getRow();
-
-        TableColumn col = tablePosition.getTableColumn();
-
-        // this gives the value in the selected cell:
-        String data = (String) col.getCellObservableValue(row).getValue();
-
-        // Se le asigna el valor obtenido previamente al text field de producto
-        txtProducto.setText(data);
     }
 
     private Boolean addProduct = false;
@@ -208,86 +143,6 @@ public class FacturacionControlador implements Initializable {
         if (addProduct) {
             AnchorPane APDetalleFactura = FXMLLoader.load(getClass().getResource("/sample/Vista/DetalleFactura.fxml"));
             APFacturacion.getChildren().setAll(APDetalleFactura);
-        }
-    }
-
-    /************* METODOS PARA OBTENER LOS VALORES DE LOS CAMPOS DE BUSQUEDA AVANZADA *************/
-
-    private String getProveedorValue() {
-        if (cmbProveedor.getSelectionModel().isEmpty()) {
-            System.out.println(cmbProveedor.getValue());
-            return "%";
-        } else {
-            return cmbProveedor.getValue();
-        }
-    }
-
-    private String getFormafarmaceuticaValue () {
-        if (cmbForma_Farmaceutica.getSelectionModel().isEmpty()) {
-            return "%";
-        } else {
-            return cmbForma_Farmaceutica.getValue();
-        }
-    }
-
-    private int getDosisValue () {
-        if (tfDosis.getText().equals("")) {
-            return 0;
-        } else if (isInteger(tfDosis.getText())) {
-            return Integer.parseInt(tfDosis.getText());
-        } else {
-            return 0;
-        }
-    }
-
-    private String getUnidadMedidaValue () {
-        if (cmbUnidadMedida.getSelectionModel().isEmpty()) {
-            return "%";
-        } else {
-            return cmbUnidadMedida.getValue();
-        }
-    }
-
-    private String getUtilidadValue () {
-        if (cmbUtilidad.getSelectionModel().isEmpty()) {
-            return "%";
-        } else {
-            return cmbUtilidad.getValue();
-        }
-    }
-
-    private String getIndicacion () {
-        if (cmbIndicacion.getSelectionModel().isEmpty()) {
-            return "%";
-        } else {
-            return cmbIndicacion.getValue();
-        }
-    }
-
-    private Boolean getGenerico () {
-        return ckboxGenerico.isSelected();
-    }
-
-    private boolean isInteger(String numero){
-        try{
-            Integer.parseInt(numero);
-            return true;
-        }catch(NumberFormatException e){
-            return false;
-        }
-    }
-
-    private int getTipo () {
-        if (!tfDosis.getText().equals("") && ckboxGenerico.isSelected()) {
-            return 0;
-        } else if (tfDosis.getText().equals("") && !ckboxGenerico.isSelected()) {
-            return 1;
-        } else if (tfDosis.getText().equals("") && ckboxGenerico.isSelected()) {
-            return 2;
-        } else if (!tfDosis.getText().equals("") && !ckboxGenerico.isSelected()) {
-            return 3;
-        } else {
-            return 0;
         }
     }
 
